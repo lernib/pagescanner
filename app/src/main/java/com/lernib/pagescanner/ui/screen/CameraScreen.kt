735216d9d0a10470cc38eac0d9546496787f290f
frqubit <ckieliszewski@lernib.com>
2024-04-03 22:01:06 -0400

    Add camera dependencies

--- app/src/main/java/com/lernib/pagescanner/ui/screen/CameraScreen.kt
+++ app/src/main/java/com/lernib/pagescanner/ui/screen/CameraScreen.kt
@@ -3,6 +3,6 @@ package com.lernib.pagescanner.ui.screen
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
-@Composable
-@Preview(showBackground = true)
-fun CameraScreen() {}
+//@Composable
+//@Preview(showBackground = true)
+//fun CameraScreen() {}
