2443494094f64fe7199050e19788ea110ba949dd
frqubit <ckieliszewski@lernib.com>
2024-04-03 21:39:06 -0400

    Add camera permission ask

--- app/src/main/java/com/lernib/pagescanner/MainActivity.kt
+++ app/src/main/java/com/lernib/pagescanner/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.lernib.pagescanner
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.lernib.pagescanner.ui.screen.HomeScreen
 import com.lernib.pagescanner.ui.theme.PageScannerTheme
@@ -24,6 +29,36 @@ class MainActivity : ComponentActivity() {
                     HomeScreen()
                 }
             }
+        }
+
+        requestCameraPermission()
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            Log.i("kilo", "Permission granted")
+        } else {
+            Log.i("kilo", "Permission denied")
+        }
+    }
+
+    private fun requestCameraPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                Log.i("kilo", "Permission previously granted")
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                android.Manifest.permission.CAMERA
+            ) -> Log.i("kilo", "Show camera permissions dialog")
+
+            else -> requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
         }
     }
 }
---
+++ app/src/main/java/com/lernib/pagescanner/ui/screen/CameraScreen.kt
@@ -0,0 +1,8 @@
+package com.lernib.pagescanner.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview(showBackground = true)
+fun CameraScreen() {}
