f13fbd326f9b4153518fe3465ff860fcc9838a3c
frqubit <ckieliszewski@lernib.com>
2024-04-03 23:10:00 -0400

    Add capture button

--- app/src/main/java/com/lernib/pagescanner/ui/component/camera/CameraPreview.kt
+++ app/src/main/java/com/lernib/pagescanner/ui/component/camera/CameraPreview.kt
@@ -2,27 +2,40 @@ package com.lernib.pagescanner.ui.component.camera
 
 import android.content.Context
 import androidx.camera.core.CameraSelector
-import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 @Composable
+@Preview(showBackground = true)
 fun CameraPreview() {
     val lensFacing = CameraSelector.LENS_FACING_BACK
     val lifecycleOwner = LocalLifecycleOwner.current
     val context = LocalContext.current
-    val preview = Preview.Builder().build()
+    val preview = androidx.camera.core.Preview.Builder().build()
     val cameraxSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
     val previewView = remember {
         PreviewView(context)
@@ -35,7 +48,31 @@ fun CameraPreview() {
         preview.setSurfaceProvider(previewView.surfaceProvider)
     }
 
-    AndroidView(factory = { previewView }, modifier = Modifier.fillMaxSize())
+    Box {
+        AndroidView(factory = { previewView }, modifier = Modifier.fillMaxSize())
+
+        Column(
+            modifier = Modifier
+                .padding(20.dp)
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
+
+            Row {
+                Spacer(modifier = Modifier.weight(1f))
+                LargeFloatingActionButton(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .size(75.dp)
+                ) {
+                    Icon(
+                        Icons.Filled.Done,
+                        "Done"
+                    )
+                }
+                Spacer(modifier = Modifier.weight(1f))
+            }
+        }
+    }
 }
 
 private suspend fun Context.getCameraProvider(): ProcessCameraProvider =
