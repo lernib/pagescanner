d4381bc0e4433e977c1be308284c32a2f3018d03
frqubit <ckieliszewski@lernib.com>
2024-04-03 20:05:15 -0400

    Add scan menu item

--- app/src/main/java/com/lernib/pagescanner/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/lernib/pagescanner/ui/screen/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.lernib.pagescanner.ui.screen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -7,9 +8,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.SmallFloatingActionButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,14 +41,27 @@ fun HomeScreen() {
 @Composable
 @Preview(showBackground = true)
 fun EditButton() {
-    SmallFloatingActionButton(
-        onClick = { /*TODO*/ },
-        modifier = Modifier
-            .size(65.dp)
-    ) {
-        Icon(
-            Icons.Filled.Edit,
-            "Add scan"
-        )
+    var expanded by remember { mutableStateOf(false) }
+
+    Box {
+        SmallFloatingActionButton(
+            onClick = { expanded = true },
+            modifier = Modifier
+                .size(65.dp)
+        ) {
+            Icon(
+                Icons.Filled.Edit,
+                "Add scan"
+            )
+        }
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                text = {  Text("Scan") },
+                onClick = { expanded = false }
+            )
+        }
     }
 }
